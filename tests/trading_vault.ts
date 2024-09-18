624c1e8fcd7339fce01065785d5208336eff9c4e
endlessbuilder <continuebuilding@gmail.com>
2024-09-18 10:00:14 -0400

    add: init vault on test

--- tests/trading_vault.ts
+++ tests/trading_vault.ts
@@ -7,10 +7,18 @@ import {
   Keypair,
   PublicKey,
   sendAndConfirmTransaction,
-  TransactionInstruction
+  TransactionInstruction,
+  SystemProgram,
+  SYSVAR_RENT_PUBKEY,
 } from '@solana/web3.js'
-import { createMint, mintTo } from '@solana/spl-token'
-import { assert } from 'chai'
+import {
+  createMint,
+  mintTo,
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+  TOKEN_PROGRAM_ID,
+} from "@solana/spl-token";
+// import { PROGRAM_ADDRESS } from '@metaplex-foundation/mpl-token-metadata'
+import { assert, util } from 'chai'
 import * as utils from './utils'
 
 import payerJson from './key/payer.json'
@@ -22,11 +30,11 @@ describe('trading_vault', () => {
   // Configure the client to use the local cluster.
   const provider = anchor.AnchorProvider.env()
   anchor.setProvider(provider)
+  let wallet = provider.wallet as anchor.Wallet
   const program = anchor.workspace.TradingVault as Program<TradingVault>
   const connection = new Connection('http://127.0.0.1:8899', 'finalized')
 
-  const payer = Keypair.fromSecretKey(Uint8Array.from(payerJson))
-  console.log('>>> create admin publickey : ', payer.publicKey.toBase58())
+  let payer= wallet.payer
 
   const leader = Keypair.fromSecretKey(Uint8Array.from(leaderJson))
   console.log('>>> create leader publickey : ', payer.publicKey.toBase58())
@@ -43,6 +51,15 @@ describe('trading_vault', () => {
   let payerUsdcATA: PublicKey
   let userUsdcATA: PublicKey
   let backendWalletUsdcATA: PublicKey
+
+  // pda
+  let vault: PublicKey
+  let vaultAuthority: PublicKey
+  let mintKeypair = new Keypair()
+  let mintAccount: PublicKey = mintKeypair.publicKey
+  let metadataAccount: PublicKey
+
+  let tokenMetadataProgram = new PublicKey("metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s")
 
   it('setup!', async () => {
     //  airdrop sol to each account
@@ -140,5 +157,58 @@ describe('trading_vault', () => {
       '>>> user USDC balance = ',
       await utils.getBalance(connection, payerUsdcATA)
     )
+
+    vault = PublicKey.findProgramAddressSync(
+      [Buffer.from("vault"), payer.publicKey.toBuffer()],
+      program.programId
+    )[0]
+    vaultAuthority = PublicKey.findProgramAddressSync(
+      [Buffer.from("vault_authority")],
+      program.programId
+    )[0]
+    mintAccount = PublicKey.findProgramAddressSync(
+      [Buffer.from("mint")],
+      program.programId
+    )[0]
+    metadataAccount = PublicKey.findProgramAddressSync(
+      [Buffer.from("metadata_account"), TOKEN_PROGRAM_ID.toBuffer(), mintAccount.toBuffer()],
+
+      program.programId
+    )[0]
+
+  })
+
+  it('vaultInitialize', async () => {
+
+    let accounts = {
+      leader: leader.publicKey,
+      backendWallet: payer.publicKey,
+      vault: vault,
+      vaultAuthority: vaultAuthority,
+      mimtAccount: mintAccount,
+      metadataAccount: metadataAccount,
+      systemProgram: SystemProgram.programId,
+      tokenProgram: TOKEN_PROGRAM_ID,
+      tokenMetadataProgram: tokenMetadataProgram,
+      rent: SYSVAR_RENT_PUBKEY,
+      associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+    }
+
+    let txSignature = await program.methods
+    .vaultInitialize()
+    .accounts(accounts)
+    .signers([leader, payer])
+    .rpc()
+
+    console.log("vaultInitialize txId = ", txSignature)
+
+  })
+
+  it('vaultInitDeposit', async () => {
+    let accounts = {
+      leader: leader.publicKey,
+      user: user.publicKey,
+      
+    }
   })
 })
